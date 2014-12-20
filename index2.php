5df16b9064e21125bec8376a15217f8372fdd97c
Yone Yone <mr.yoneyone17@gmail.com>
2014-12-20 03:49:39 -0500

    first initialization

---
+++ index2.php
@@ -0,0 +1,191 @@
+<html>
+
+	<head>
+		
+		<title>Welcome to Baganhub B2B Social Network</title>
+		<link rel="stylesheet" media="screen" type="text/css" href="css/index2.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="UTF-8">
+		<script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
+		
+	</head>
+	
+	<body>
+	
+		<div id="nav_title">
+			
+			<div id="nav_title_inner">
+			
+				<div id="bghlogo">
+					
+					<img src="sitelogo/logo.png" class="header_logo"></img>
+					
+				</div>
+				
+				<script>
+					function login(){
+						jQuery('#login').slideToggle();
+					}
+				</script>
+				
+				<a onclick="login()" class="reslogin">Login</a>
+				
+				<div id="login">
+					
+					<div class="forrespon">
+					
+						<span class="block"><div class="typpo">Email</div>:<input name="user_email" id="user_email" type="text" class="fill">&nbsp;&nbsp;</span>
+						
+						<span class="block"><div class="typpo">Password</div>:<input name="user_password" id="user_password" type="password" class="fill">&nbsp;&nbsp;</span>
+						
+						<span class="block"><input type="submit" id="btn_login" name="login" value="Sign In"></span>
+				
+					</div>
+				
+				</div>
+			
+			</div>
+			
+		</div>
+	
+	
+		<div id="content_area" class="cont_log">
+			
+			<div id="content_area_inner" class="login_area">
+			
+				<div class="sign_upbox">
+					
+					<div class="sign_title">Join BaganHub Today, Please Sign Up</div>
+					<div class="underline"></div>
+
+					<div class="sign_upform">
+						
+						<input type="text" placeholder="First name" class="form">
+						<input type="text" placeholder="Last name" class="form">
+						<input type="email" placeholder="Email" class="form">
+						<input type="password" placeholder="Password" class="form">
+						<span>6 or more characters</span>
+						<p class="signup"><input type="submit" value="SIGN UP"></p>
+						
+					</div>
+						
+				</div>
+			
+			</div>
+			
+		</div>
+		
+		<div id="content_area" class="cont">
+		
+			<div id="content_area_inner">
+				
+				<p class="btitle"><span class="b2b">B2B</span> Social Network for Local and Global Companies</p>
+				
+				<ul class="pre_desc">
+					
+					<li class="desc_box">
+						
+						<img class="desc_img" src="images/social.png">
+						<h3 class="desc_title">B2B Social Network</h3>
+						<p>Connect with business partner and grow<br/>your network.</p>
+						<p>လုပ္ငန္းရွင္မ်ားျဖင့္ မိတ္ေဆြ ဖြဲ႕ၿပီးသင့္ Network ကုိတိုးခ်ဲ႕ပါ။</p>
+						
+					</li>
+					
+					<li class="desc_box">
+					
+						<img class="desc_img" src="images/market.png">
+						<h3 class="desc_title">Market Place</h3>
+						<p>Submit what you offer to other business and find<br/>what you need for your business.</p>
+						<p>တျခားလုပ္ငန္းေတြ အတြက္ သင္ေရာင္းတဲ့ ကုန္ပစၥည္း (သုိ႕) <br/>၀န္ေဆာင္မႈ ကုိ Submit လုပ္ႏိုင္သလုိ သင့္လုပ္ငန္းအတြက္ <br/>လိုအပ္တာေတြကုိလည္း ရွာႏိုင္ပါတယ္။</p>
+						
+					</li>
+					
+					<li class="desc_box">
+					
+						<img class="desc_img" src="images/company.png">	
+						<h3 class="desc_title">Business Page</h3>
+						<p>Submit your business profile and get<br/>your business page to interact with your clients<br/>about your updated news and activities.</p>
+						<p>သင့္လုပ္ငန္းအေသးစိတ္ကုိ Submit လုပ္ၿပီးကုိယ္ပိုင္ <br/>Business Page ကုိရယူပါ။ သင့္လုပ္ငန္းလုပ္ေဆာင္မႈ <br/>သတင္းမ်ားကုိ Clients ေတြနဲ႕ဆက္သြယ္ႏိုင္ပါတယ္။</p>
+						
+					</li>
+					
+				</ul>
+			
+			</div>
+		
+		</div>
+		
+		<div id="content_area" class="cont1">
+		
+			<div id="content_area_inner">
+					
+					<div class="nazi"></div>
+			
+					<p class="mid" style="float:left;"><img src="images/probvssol.png"></p>
+					
+					<div class="nazi"></div>
+			
+			</div>
+		
+		</div>
+		
+		<div id="line2"></div>
+		
+		<div id="content_area" class="cont">
+		
+			<div id="content_area_inner" class="join">
+			
+			
+					<p class="half"><img src="images/whybaganhub.png"></p>
+					<div id="joinbh">
+						
+						<div class="lineicon">
+							<p>Grow business and professional partners.</p>
+						</div>
+						
+						<div class="lineicon">
+							<p>Grow business knowledge.</p>
+						</div>
+						
+						<div class="lineicon">
+							<p>Grow more clients and increase sales.</p>
+						</div>
+						
+						<div class="lineicon">
+							<p>Grow customer relationship.</p>
+						</div>
+						
+					</div>
+			
+			</div>
+		
+		</div>
+	
+		<div id="footer">
+		
+			<div id="inner_footer">
+				
+				<ul class="footer_menu_bar">
+					
+					<li>Contact Us</li>
+					<li>About Us</li>
+					
+				</ul>
+				
+				<span class="site_copyright"><a href="http://connection420.com/copyright-policy.php">BaganHub © <?php echo date(Y)?></a></span>
+				
+			</div>
+
+		</div>
+	
+	</body>
+
+</html>
+
+
+
+
+
+
+
